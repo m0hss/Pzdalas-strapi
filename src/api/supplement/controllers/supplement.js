b3f59897afdb06abec026478af36626d0ad16591
m0hss <med.sassi@outlook.be>
2023-12-09 14:36:58 +0100

    first commit

---
+++ src/api/supplement/controllers/supplement.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * supplement controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::supplement.supplement');
---
+++ src/api/supplement/routes/supplement.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * supplement router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::supplement.supplement');
